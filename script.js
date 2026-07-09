25b5331571d051cec14e4a06877dd57c4aca572b
nourahAs05-eng <nourah5413@gmail.com>
2026-07-09 05:34:40 +0300

    fix cd button and responsive design

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
 const cd = document.getElementById("cdButton");
 const music = document.getElementById("music");
 
-cd.addEventListener("click", async function () {
+cd.addEventListener("click", function () {
 
     if (music.paused) {
 
-        try {
-            await music.play();
-
-            cd.querySelector("img").classList.add("playing");
-
-        } catch (error) {
-            console.log("Audio error:", error);
-        }
+        music.play();
+        cd.querySelector("img").classList.add("playing");
 
     } else {
 
         music.pause();
-
         cd.querySelector("img").classList.remove("playing");
 
     }
